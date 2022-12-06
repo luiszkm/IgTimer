7feeb1f2dfc2bdbe9ff86b5df3e05371c8dd6ab6
luiszkm <luiszkm@gmail.com>
2022-12-06 20:45:45 -0300

    add config tailwind

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {App} from './App'
 import { Header } from './components/Header'
+import { DefaultLAyout } from './layout/DefaultLayout'
 import './styles/global.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Header/>
-    <App />
+   <DefaultLAyout />
   </React.StrictMode>
 )
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,14 +2,44 @@
 module.exports = {
   content: ['./src/**/*.tsx'],
   theme: {
-    extend: {
-      colors:{
-        green:{
-          "dark": "#015F43",
-          "normal": "#00875F",
-          "light": "#00B37E"
-        }
+
+    fontSize: {
+      xs: 12,
+      sm: 14,
+      md: 16,
+      lg: 18,
+      xl: 24,
+      'mega': 160,
+    },
+    colors: {
+      transparent: 'transparent',
+      'black': '#000000',
+      'white': '#FFFFFF',
+
+      green: {
+        800: "#015F43",
+        500: "#00875F",
+        300: "#00B37E"
+      },
+      gray: {
+        900: '#121214',
+        800: '#202024',
+        700: '#323238',
+        500: '#7C7C8A',
+        300: '#8D8D99',
+        200: '#C4C4CC',
+        100: '#E1E1E6',
+      },
+      red: {
+        900: '#00875F',
+        500: '#F03847',
       }
+    }
+  },
+  extend: {
+    fontFamily: {
+      roboto: 'Roboto sans- serif',
+      robotoMono: 'Roboto Mono monospace',
     },
   },
   plugins: [],
